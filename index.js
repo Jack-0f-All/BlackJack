25afee72834041f6b5f0f69554f7d0b5346b1abc
Jack-0f-All <jackofalllearning@gmail.com>
2024-08-27 15:09:45 -0500

    Add gameDeck and shuffle() functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let player = {
     name: "Per",
     chips: 200
 }
-let cards = []
+let hand = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
@@ -12,26 +12,54 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
-
+let deckEl = document.getElementById("deck-el")
 playerEl.textContent = player.name + ": $" + player.chips
 
 
-let standard_deck = [1,1,1,1,
-            2,2,2,2,
-            3,3,3,3,
-            4,4,4,4,
-            5,5,5,5,
-            6,6,6,6,
-            7,7,7,7,
-            8,8,8,8,
-            9,9,9,9,
-            10,10,10,10, //10's
-            10,10,10,10, //Jacks
-            10,10,10,10, //Queen
-            10,10,10,10] //King
+let standardDeck = []
+let gameDeck = []
 
+function resetDeck(){
+    standardDeck = [1,1,1,1,
+        2,2,2,2,
+        3,3,3,3,
+        4,4,4,4,
+        5,5,5,5,
+        6,6,6,6,
+        7,7,7,7,
+        8,8,8,8,
+        9,9,9,9,
+        10,10,10,10, //10's
+        10,10,10,10, //Jacks
+        10,10,10,10, //Queen
+        10,10,10,10] //King
+}
 
-/* Rewrite both functions to utalize actual deck logic*/
+function shuffleCards(){
+    let randomCard = 0
+    let firstFive = ""
+    let randomIndex = 0
+
+    gameDeck = []
+    resetDeck()
+
+    for(let i = 0; i<52; i++){
+        randomIndex = Math.floor( Math.random()*standardDeck.length)
+        gameDeck.push(standardDeck[randomIndex])
+        standardDeck.splice(randomIndex, 1)
+        
+
+    }
+
+    for(let i = 0; i<5; i++){
+        firstFive += gameDeck[i] + " "
+    }
+
+    deckEl.textContent = "Deck = " + firstFive
+    
+        }
+
+/* Rewrite both functions to utalize actual deck logic
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
@@ -42,32 +70,35 @@ function getRandomCard() {
         return randomNumber
     }
 }
+*/
+
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
+        
+        if(gameDeck.length<1)
+            shuffleCards()
+
+        let card = gameDeck.pop()
         sum += card
-        cards.push(card)
+        hand.push(card)
         renderGame()        
     }
 }
-//-------------------------------------------------
-
-
-
 
 function startGame() {
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
+    shuffleCards()
+    let firstCard = gameDeck.pop()
+    let secondCard = gameDeck.pop()
+    hand = [firstCard, secondCard]
     sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    for (let i = 0; i < hand.length; i++) {
+        cardsEl.textContent += hand[i] + " "
     }
     
     sumEl.textContent = "Sum: " + sum
